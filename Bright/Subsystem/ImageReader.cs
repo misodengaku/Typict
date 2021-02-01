786a1ca7ae8e3c0c0387d2b2089ac8cbd7b2b0b7
misodengaku <misodengaku@gmail.com>
2021-02-02 01:21:24 +0900

    remove Susie plugin integration

--- Bright/Subsystem/ImageReader.cs
+++ Bright/Subsystem/ImageReader.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
-using SpiMngx;
 using System.Drawing.Imaging;
 
 namespace Bright.Subsystem
 {
-    public class ImageReader : IDisposable
+    public class ImageReader
     {
         #region Singleton
         static ImageReader instance = null;
@@ -23,25 +22,9 @@ namespace Bright.Subsystem
         }
         #endregion
 
-        public SpiManager SusieManager;
-        public ImageReader()
-        {
-            SusieManager = new SpiManager(LoadFlag.Default & ~LoadFlag.Load00AM);
-            SusieManager.LoadAll();
-        }
-
-        public void Dispose()
-        {
-            if (SusieManager != null)
-                SusieManager.Dispose();
-            SusieManager = null;
-        }
-
         public IEnumerable<string> AcceptExtents()
         {
             foreach (var ext in GetSystemAcceptExt())
-                yield return ext;
-            foreach (var ext in GetSusieAcceptExt())
                 yield return ext;
         }
 
@@ -62,25 +45,6 @@ namespace Bright.Subsystem
             }
         }
 
-        /// <summary>
-        /// Susie Plug-inで読み込める画像フォーマットの拡張子を取得します。
-        /// </summary>
-        /// <returns>読み込める拡張子のString配列</returns>
-        public IEnumerable<string> GetSusieAcceptExt()
-        {
-            // all images/arcives|*....|JPEG...|*.jpg...
-            //               　　↑ここから取得(対応形式全てがセミコロン区切りで取得できる)
-            string SusieExt = this.SusieManager.GetFileFilter();
-            //対応形式全てのセミコロン区切りを取得する
-            if (String.IsNullOrEmpty(SusieExt))
-                yield break;
-
-            SusieExt = SusieExt.Split(new char[] { '|' })[1];
-
-            foreach (string addext in SusieExt.Split(new char[] { ';' }))
-                yield return addext;
-        }
-
         public Image LoadImage(string path)
         {
             Image loaded = null;
@@ -88,32 +52,11 @@ namespace Bright.Subsystem
                 //ファイルが見つからない
                 throw new System.IO.FileNotFoundException("Image not found:" + path);
 
-            loaded = SusieImageReader(path);
-            if (loaded == null)
-            {
-                loaded = K.Snippets.Images.ImageSafeReader(path, false);
-            }
+            loaded = K.Snippets.Images.ImageSafeReader(path, false);
 
             if (loaded == null)
                 throw new Exception("Image cannot read");
             return loaded;
-        }
-
-        /// <summary>
-        /// 画像をSusieプラグインから読み込みます。
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        private Image SusieImageReader(string path)
-        {
-            if (SusieManager.Plugins.Count == 0)
-                return null;
-            using (ImagePlugin ip = SusieManager.QueryImagePlugin(path))
-            {
-                if (ip == null)
-                    return null;
-                return ip.GetPicture(path);
-            }
         }
 
         public Image CreateThumbnail(string path)
@@ -123,12 +66,7 @@ namespace Bright.Subsystem
                 //ファイルが見つからない
                 throw new System.IO.FileNotFoundException("Image not found:" + path);
 
-            loaded = SusieThumbnailReader(path);
-            if (loaded == null)
-                loaded = K.Snippets.Images.ImageSafeReader(path, false);
-
-            if (loaded == null)
-                loaded = SusieImageReader(path);
+            loaded = K.Snippets.Images.ImageSafeReader(path, false);
 
             if (loaded == null)
                 throw new Exception("Image cannot read");
@@ -145,23 +83,5 @@ namespace Bright.Subsystem
                 () => false, IntPtr.Zero);
         }
 
-        private Image SusieThumbnailReader(string path)
-        {
-            if (SusieManager.Plugins.Count == 0)
-                return null;
-            using (ImagePlugin ip = SusieManager.QueryImagePlugin(path))
-            {
-                if (ip == null)
-                    return null;
-                return ip.GetPreview(path);
-            }
-        }
-
-        ~ImageReader()
-        {
-            if (SusieManager != null)
-                SusieManager.Dispose();
-            SusieManager = null;
-        }
     }
 }
